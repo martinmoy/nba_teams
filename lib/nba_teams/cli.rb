2b719a6ed3c5d59686079f526eededdadc508a7f
Martin Moy <martinmoy1991@gmail.com>
2021-03-18 09:31:58 +0000

    cli adding 3 method

--- lib/nba_teams/cli.rb
+++ lib/nba_teams/cli.rb
@@ -1,7 +1,30 @@
 class NbaTeams::CLI 
   
   def call 
-    puts "Welcome"
+    intro 
+    list_teams
+    get_user_input 
+    
   end
+  
+  def intro 
+    current_year = Time.now.year
+    puts "Welcome to #{current_year}'s NBA Roster"
+    puts "Here is the teams in the NBA"
+    puts "Which team's roster would you to browse? "
+  end
+  
+  def list_teams
+    @teams = ["Hawks", "Knicks", "Hornets"]
+    @teams.each.with_index(1) do |team, index|
+      puts "#{index}. #{team}"
+    end
+  end
+  
+  def get_user_input 
+    
+  end
+  
+ 
   
 end
