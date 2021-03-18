8c09289a05870f633c347576230be6ab15908108
Martin Moy <martinmoy1991@gmail.com>
2021-03-18 10:06:41 +0000

    cli basic done

--- lib/nba_teams/cli.rb
+++ lib/nba_teams/cli.rb
@@ -2,10 +2,10 @@ class NbaTeams::CLI
   
   def call 
     intro 
-    scrape_teams
     list_teams
     select_team
-    
+    select_player
+    goodbye
   end
   
   def intro 
@@ -13,6 +13,7 @@ class NbaTeams::CLI
     puts "Welcome to #{current_year}'s NBA Roster"
     puts "Here is the teams in the NBA"
     puts "Which team's roster would you to browse? "
+    scrape_teams
   end
   
   def scrape_teams
@@ -20,6 +21,7 @@ class NbaTeams::CLI
   end
   
   def list_teams
+    puts " "
     @teams.each.with_index(1) do |team, index|
       puts "#{index}. #{team}"
     end
@@ -29,6 +31,7 @@ class NbaTeams::CLI
   def select_team 
     input = gets.strip.to_i
     if valid_input(input, @teams)
+      scrape_roster
       list_roster
     else
       puts "Please input a number from 1 to #{@teams.size}"
@@ -40,8 +43,44 @@ class NbaTeams::CLI
     input.to_i <= data.length && input.to_i > 0
   end
   
-  def 
+  def scrape_roster
+    @roster = ["WADE", "Kobe", "Lebron", "Hello Kitty"]
+  end
   
- 
+  def list_roster
+    puts " "
+    puts " "
+    @roster.each.with_index(1) do |player, index|
+      puts "#{index}. #{player}"
+    end
+    puts " "
+    puts "Please input the number for the player you want to view."
+  end
+  
+  def select_player
+    input = gets.strip.to_i
+    if valid_input(input, @roster)
+      scrape_stats
+      list_stats
+    else
+      puts "Please input a number from 1 to #{@roster.size}"
+      select_player
+    end
+  end
+  
+  def scrape_stats
+    @stat = ["chicken", " game"]
+  end
+  
+  def list_stats
+    @stat.each.with_index(1) do |stats, index|
+      puts "#{index}. #{stats}"
+    end
+  end
+  
+  def goodbye
+    puts " "
+    puts "see you later"
+  end
   
 end
