60e4677348c28d704ccffb30636c12f25b39d28e
Martin Moy <martinmoy1991@gmail.com>
2021-03-18 10:15:10 +0000

    team method added

--- lib/nba_teams/team.rb
+++ lib/nba_teams/team.rb
@@ -1,2 +1,22 @@
 class NbaTeams::Team 
+  attr_accessor :name, :url, :player
+  @@all = []
+  
+  def initialize(name, url)
+    @name = name
+    @url = url
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def player 
+  end
+  
 end
